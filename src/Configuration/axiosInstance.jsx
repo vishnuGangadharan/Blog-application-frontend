2bb11c539a0802087691b6b4c82cebdbc7d75dbe
vishnuGangadharan <pvishnu997@gmail.com>
2024-10-07 17:01:17 +0530

    updated

--- src/Configuration/axiosInstance.jsx
+++ src/Configuration/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/user', 
+  baseURL: 'https://blog-application-backend-o31x.onrender.com/user', 
   headers: {
     'Content-Type': 'application/json',
   },
