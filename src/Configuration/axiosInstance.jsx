a4a428d24919eccefa3b9ead6821e3f3747651a4
vishnuGangadharan <pvishnu997@gmail.com>
2024-10-13 08:50:20 +0530

    cors

--- src/Configuration/axiosInstance.jsx
+++ src/Configuration/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://blog-application-backend-o31x.onrender.com/user',
   headers: {
     'Content-Type': 'application/json',
   },
